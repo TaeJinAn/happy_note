13bfb07972846a87c88d1fe91e25900f6a0abf68
taejin.ahn <xowls0460@gmail.com>
2024-09-08 17:39:50 +0900

    form 적용 날짜와 콘텐츠 비어있시 alert처리

--- src/components/WriteTodoForm.js
+++ src/components/WriteTodoForm.js
@@ -5,12 +5,25 @@ import { Button, TextField } from "@mui/material";
 export default function WriteTodoForm() {
   const onSubmit = (e) => {
     e.preventDefault();
+    const form = e.target;
+    if (form.regDate.value.length == 0) {
+      alert("언제 해야 하는지 알려 주세요.");
+    }
+    if (form.content.value.length == 0) {
+      alert("무엇을 해야 하는지 알려 주세요.");
+    }
   };
   return (
     <>
       <form onSubmit={onSubmit} className="flex flex-1 gap-5 flex-col p-16">
-        <TextField type="datetime-local" label="언제 해야 되나요?" focused />
         <TextField
+          type="datetime-local"
+          label="언제 해야 되나요?"
+          focused
+          name="regDate"
+        />
+        <TextField
+          name="content"
           type="text"
           label="무엇을 해야 하나요?"
           multiline
@@ -20,9 +33,9 @@ export default function WriteTodoForm() {
             htmlInput: { className: "flex-1" },
           }}
         />
-        <Button variant="contained">
+        <Button type="submit" variant="contained">
           {/* <i class="fa-solid fa-marker"></i> */}
-          <FontAwesomeIcon icon={faMarker} className="mr-2"/>
+          <FontAwesomeIcon icon={faMarker} className="mr-2" />
           <span>할일추가</span>
         </Button>
       </form>
